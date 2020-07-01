09199cdc40351f01986189d854ab83248a1b3312
pjflux2001 <46859995+pjflux2001@users.noreply.github.com>
2020-07-01 15:44:15 +0530

    start

---
+++ news.js
@@ -0,0 +1,11 @@
+var requestOptions = {
+  method: 'GET',
+  redirect: 'follow'
+};
+
+fetch("http://api.coronatracker.com/news/trending?limit=10&offset&country=Spain&countryCode", requestOptions)
+  .then(response => response.text())
+  .then(result => document.getElementById("news").innerHTML = result)
+  .catch(error => console.log('error', error));
+
+
