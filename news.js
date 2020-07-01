b68b932791cdfdcb5bd8a9a3e0adacc748b631a3
pjflux2001 <46859995+pjflux2001@users.noreply.github.com>
2020-07-01 16:09:12 +0530

    Update news.js

--- news.js
+++ news.js
@@ -3,7 +3,7 @@ var requestOptions = {
   redirect: 'follow'
 };
 
-fetch("http://api.coronatracker.com/news/trending?limit=10&offset&country=Spain&countryCode", requestOptions)
+fetch("https://api.coronatracker.com/news/trending?limit=10&offset&country=Spain&countryCode", requestOptions)
   .then(response => response.text())
   .then(result => document.getElementById("news").innerHTML = result)
   .catch(error => console.log('error', error));
