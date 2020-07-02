03884e115010e6fee0b2c509f14c08a7146c46a2
Barthwal <barthwalumang@gmail.com>
2020-07-03 00:20:46 +0530

    add more fields to donor form

--- form_donor.js
+++ form_donor.js
@@ -6,13 +6,29 @@
   
   $(add_donor).click(function(){
     var donor = {
-      id: $("#v1").val() + Date.now(),
+      //id: $("#v1").val() + Date.now(),
+      id: Date.now(),
+      date: Date(Date.now()),
+      //date: $("#v1").val(),
+      //time: $("#v2").val(),
       
-      date: $("#v1").val(),
-      time: $("#v2").val(),
-      
-      contact: $("#v5").val(),
-      comments: $("#v6").val()
+      //contact: $("#v5").val(),
+      //comments: $("#v6").val(),
+
+      donorName: $("#v7").val(),
+      mobileNumber: $("#v8").val(),
+      emailAddress: $("#v9").val(),
+      age: $("#v10").val(),
+      gender: $("#v11").val(),
+      city: $("#v12").val(),
+      country: $("#v13").val(),
+      bloodGroup: $("#v14").val(),
+      diabetes: $("#v15").val(),
+      liver: $("#v16").val(),
+      kidney: $("#v17").val(),
+      lung: $("#v18").val(),
+      bloodPressure: $("#v19").val(),
+      recoveryDate: $("#v20").val()
     };
   addDonor(donor);
   alert("Created!");
@@ -27,10 +43,23 @@
   
         snapshot.forEach(function(childsnapshot){
           var item = childsnapshot.val();
-          donorHTMLitem += "<div class'donorHTMLitem'><hr><ul></div>";
-          donorHTMLitem += "<li> Time : <span>"+ item.time + "</span></li>"
-          donorHTMLitem += "<li> Date : <span>"+ item.date + "</span></li>"
-          donorHTMLitem += "<li> Contact : <span>"+ item.contact + "</span></li>"
+          donorHTMLitem += "<div class'patientHTMLitem'><hr><ul></div>";
+          //patientHTMLitem += "<li> Time : <span>"+ item.time + "</span></li>"
+          donorHTMLitem += "<li><b> Date : </b><span>"+ item.date + "</span></li>"
+          //patientHTMLitem += "<li> Contact : <span>"+ item.contact + "</span></li>"
+          donorHTMLitem += "<li><b> Patient Name : </b><span>"+ item.patientName + "</span></li>"
+          donorHTMLitem += "<li><b> Mobile Number : </b><span>"+ item.mobileNumber + "</span></li>"
+          donorHTMLitem += "<li><b> Email : </b><span>"+ item.emailAddress + "</span></li>"
+          donorHTMLitem += "<li><b> Age : </b><span>"+ item.age + "</span></li>"
+          donorHTMLitem += "<li><b> Gender : </b><span>"+ item.gender + "</span></li>"
+          donorHTMLitem += "<li><b> City : </b><span>"+ item.city + "</span></li>"
+          donorHTMLitem += "<li><b> Country : </b><span>"+ item.country + "</span></li>"
+          donorHTMLitem += "<li><b> Blood Group : </b><span>"+ item.bloodGroup + "</span></li>"
+          donorHTMLitem += "<li><b> Diabetes : </b><span>"+ item.diabetes + "</span></li>"
+          donorHTMLitem += "<li><b> Liver Disease : </b><span>"+ item.liver + "</span></li>"
+          donorHTMLitem += "<li><b> Kidney Disease : </b><span>"+ item.kidney + "</span></li>"
+          donorHTMLitem += "<li><b> Blood Pressure Problem : </b><span>"+ item.bloodPressure + "</span></li>"
+          donorHTMLitem += "<li><b> Recovery Date : </b><span>"+ item.recoveryDate + "</span></li>"
           donorHTMLitem += "</ul></div>";
           donorHTMLitem += "<button type='button' class='btn btn-primary delete-donor' id="+item.id+">Delete Donor</button>";
   
