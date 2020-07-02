a405169c81974c00eca9c2fbb081c1340ef3a24d
Barthwal <barthwalumang@gmail.com>
2020-07-02 21:48:53 +0530

    Add more fields to patient form

--- form_patient.js
+++ form_patient.js
@@ -5,13 +5,24 @@
   
   $(add_patient).click(function(){
     var patient = {
-      id: $("#v1").val() + Date.now(),
+      //id: $("#v1").val() + Date.now(),
+      id: Date.now(),
+      date: Date(Date.now()),
+      //date: $("#v1").val(),
+      //time: $("#v2").val(time.now()),
       
-      date: $("#v1").val(),
-      time: $("#v2").val(),
-      
-      contact: $("#v5").val(),
-      comments: $("#v6").val()
+      //contact: $("#v5").val(),
+      //comments: $("#v6").val(),
+
+      patientName: $("#v7").val(),
+      mobileNumber: $("#v8").val(),
+      emailAddress: $("#v9").val(),
+      age: $("#v10").val(),
+      gender: $("#v11").val(),
+      city: $("#v12").val(),
+      country: $("#v13").val(),
+      hospitalName: $("#v14").val(),
+      bloodGroup: $("#v15").val()
     };
   addpatient(patient);
   alert("Created!");
@@ -25,11 +36,20 @@
         snapshot.forEach(function(childsnapshot){
           var item = childsnapshot.val();
           patientHTMLitem += "<div class'patientHTMLitem'><hr><ul></div>";
-          patientHTMLitem += "<li> Time : <span>"+ item.time + "</span></li>"
-          patientHTMLitem += "<li> Date : <span>"+ item.date + "</span></li>"
-          patientHTMLitem += "<li> Contact : <span>"+ item.contact + "</span></li>"
+          //patientHTMLitem += "<li> Time : <span>"+ item.time + "</span></li>"
+          patientHTMLitem += "<li><b> Date : </b><span>"+ item.date + "</span></li>"
+          //patientHTMLitem += "<li> Contact : <span>"+ item.contact + "</span></li>"
+          patientHTMLitem += "<li><b> Patient Name : </b><span>"+ item.patientName + "</span></li>"
+          patientHTMLitem += "<li><b> Mobile Number : </b><span>"+ item.mobileNumber + "</span></li>"
+          patientHTMLitem += "<li><b> Email : </b><span>"+ item.emailAddress + "</span></li>"
+          patientHTMLitem += "<li><b> Age : </b><span>"+ item.age + "</span></li>"
+          patientHTMLitem += "<li><b> Gender : </b><span>"+ item.gender + "</span></li>"
+          patientHTMLitem += "<li><b> Hospital Name : </b><span>"+ item.hospitalName + "</span></li>"
+          patientHTMLitem += "<li><b> City : </b><span>"+ item.city + "</span></li>"
+          patientHTMLitem += "<li><b> Country : </b><span>"+ item.country + "</span></li>"
+          patientHTMLitem += "<li><b> Blood Group : </b><span>"+ item.bloodGroup + "</span></li>"
           patientHTMLitem += "</ul></div>";
-          patientHTMLitem += "<button type='button' class='btn btn-primary delete-patient' id="+item.id+">Delete Cab</button>";
+          patientHTMLitem += "<button type='button' class='btn btn-primary delete-patient' id="+item.id+">Delete Patient</button>";
   
       });
       $("#show-patient").html(patientHTMLitem);
