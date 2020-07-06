623cd013f0f5e77dd3ee65e231f912cf877fc6fa
pjflux2001 <pj.flux2001@gmail.com>
2020-07-06 12:31:22 +0530

    numbers added

--- geo.js
+++ geo.js
@@ -6,9 +6,11 @@ function getLocation() {
   } else { 
     x.innerHTML = "Geolocation is not supported by this browser.";
   }
+  
 }
 
 function showPosition(position) {
   x.innerHTML = "Latitude: " + position.coords.latitude + 
   "<br>Longitude: " + position.coords.longitude;
 }
+
