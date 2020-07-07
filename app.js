ec60c68f87a0bc664d5718ad88d8055c4ead3dda
pjflux2001 <pj.flux2001@gmail.com>
2020-07-07 21:10:39 +0530

    sendgrid init

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 var express = require("express"),
 	app = express(),
 	bodyParser = require("body-parser"),
-	mongoose = require("mongoose")
-
+	mongoose = require("mongoose"),
+	sgMail = require('@sendgrid/mail');
 //==========================
 //SOME OTHER PACKAGES CONFIG
 //==========================
@@ -20,6 +20,21 @@ app.use(bodyParser.json());
 app.get("/",function(req,res){
 	res.render("index");
 });
+
+// using Twilio SendGrid's v3 Node.js Library
+// https://github.com/sendgrid/sendgrid-nodejs
+
+sgMail.setApiKey("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");
+const msg = {
+  to: 'f20180332@goa.bits-pilani.ac.in',
+  from: 'pj.flux2001@gmail.com',
+  subject: 'Sending with Twilio SendGrid is Fun',
+  text: 'and easy to do anywhere, even with Node.js',
+  html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+};
+sgMail.send(msg);
+
+ 
 
 //LISTENER PROCESS
 var port = process.env.PORT || 31000
