e0d1c7d705ee2e231ba1164097ed844c8a12f174
Sudhanshu Mohan <sudhanshumohan781@gmail.com>
2020-07-08 13:59:17 +0530

    Added A Footer to each file

--- public/news1.js
+++ public/news1.js
@@ -1,15 +1,3 @@
-//hamburger button 
-document.querySelector("body > div.ui.pointing.menu.stackable > div.hamburger").addEventListener("click", function(e) { 
-		$menu = $(this).parent();
-		if(!$(this).hasClass('active')) {
-			$(this).addClass('active');
-			$menu.addClass('open');
-		} else {
-			$(this).removeClass('active');
-			$menu.removeClass('open');
-		}
-		e.preventDefault();
-	});
 var requestOptions = {
   method: 'GET',
   redirect: 'follow'
