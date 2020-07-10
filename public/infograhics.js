98245f58d1c1ff2920843f14f42793317fc16c08
Barthwal <barthwalumang@gmail.com>
2020-07-10 20:20:05 +0530

    fetching infographics url

--- public/infograhics.js
+++ public/infograhics.js
@@ -1,8 +1,11 @@
+//const { response } = require("express");
+
 var settings = {
 	"async": true,
 	"crossDomain": true,
 	"url": "https://bing-image-search1.p.rapidapi.com/images/search?q=covid%20infographic",
 	"method": "GET",
+	//"dataType": 'JSON',
 	"headers": {
 		"x-rapidapi-host": "bing-image-search1.p.rapidapi.com",
 		"x-rapidapi-key": "e94bf9e7a6mshf66f488d77fd452p116c67jsn2282a1fe240f"
@@ -10,5 +13,10 @@ var settings = {
 }
 
 $.ajax(settings).done(function (response) {
+
+	for(i=1; i<35; i++){
+	document.getElementById("val").innerHTML += "<p>"+ response.value[i].thumbnailUrl + "</p>"
+	//document.getElementById("val").innerHTML += "<div class='carousel-item active'>" + "<img src=\""+ response.value[0].thumbnailUrl +"\" class='d-block w-100 h-100'</img>" + "</div>" + "<div class='carousel-item'>" + "<img src=\""+ response.value[i].thumbnailUrl +"\" class='d-block w-100 h-100'</img>" + "</div>";
+	}
 	console.log(response);
 });
