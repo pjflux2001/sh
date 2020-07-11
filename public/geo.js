75bb0577bb520f97362aa78410f97f00b8dc8f5d
pjflux2001 <pj.flux2001@gmail.com>
2020-07-11 17:39:19 +0530

    track amenities connected to api

--- public/geo.js
+++ public/geo.js
@@ -1,5 +1,5 @@
 var x = document.getElementById("geoloc");
-
+var browserPos;
 function getLocation() {
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(showPosition);
@@ -10,7 +10,33 @@ function getLocation() {
 }
 
 function showPosition(position) {
-  x.innerHTML = "Latitude: " + position.coords.latitude + 
-  "<br>Longitude: " + position.coords.longitude;
+
+  browserPos = {lat: position.coords.latitude, lon: position.coords.longitude};
+  x.innerHTML = "&ensp; Your Position -&ensp;Latitude: " + position.coords.latitude + 
+  "&ensp;&&&ensp;Longitude: " + position.coords.longitude;
 }
 
+getLocation();
+
+var platform = new H.service.Platform({
+  'apikey': "rMWNJfAV2zeVVEYh5jOh8Lk4Qka34bkEmpOszDwDoik"
+});
+var geocoder = platform.getSearchService();
+function geocodeAndSearch(){
+  var rad = parseInt(document.getElementById("slider_value").innerHTML);
+  var query = document.getElementById("service").value;
+  let geocodeParam ={
+      q: query ,
+      in: 'circle:'+ browserPos.lat +',' + browserPos.lon +';r='+rad,
+      limit: 50
+  }
+  function onResult(result){
+      console.log(result);
+
+      console.log(result.items.length + " " + query +" Found in " + rad + " meters");        
+      //info bubble
+               
+  }
+  geocoder.discover(geocodeParam,onResult, alert);
+                
+}
