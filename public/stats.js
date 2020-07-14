637a375a28b3a5e5375d28c777f2c37485c1934d
Barthwal <barthwalumang@gmail.com>
2020-07-15 01:14:51 +0530

    link stats api to index

--- public/stats.js
+++ public/stats.js
@@ -1,7 +1,3 @@
-function showstats(){
-
-var x = document.getElementById("stats");
-
 var myHeaders = new Headers();
 myHeaders.append("Subscription-Key", "e4db10f1ac324ea7a370a0f2564f800e");
 
@@ -16,18 +12,17 @@ $(function() {
    function(json) {
     fetch("https://api.smartable.ai/coronavirus/stats/"+json.countryCode, requestOptions)
     .then(response => response.json())
-    .then(result => x.innerHTML = "Country : "+result.location.countryOrRegion + " [ "+result.location.isoCode +" ]"+"<br>Confirmed : " + result.stats.totalConfirmedCases + " ( +" + result.stats.newlyConfirmedCases +" )"+ '<br>' +"Recovered : " + result.stats.totalRecoveredCases + " ( +" + result.stats.newlyRecoveredCases +" )"+ "<br>Deaths : " + result.stats.totalDeaths + " ( +" + result.stats.newDeaths +" )"+ "<br>Last Updated on " + result.updatedDateTime.replace('T'," @ ").replace('Z'," ").replace(".",":")+ "<br><br><i> + shows new cases reported yesterday</i>")
+    //.then(result => x.innerHTML = "Country : "+result.location.countryOrRegion + " [ "+result.location.isoCode +" ]"+"<br>Confirmed : " + result.stats.totalConfirmedCases + " ( +" + result.stats.newlyConfirmedCases +" )"+ '<br>' +"Recovered : " + result.stats.totalRecoveredCases + " ( +" + result.stats.newlyRecoveredCases +" )"+ "<br>Deaths : " + result.stats.totalDeaths + " ( +" + result.stats.newDeaths +" )"+ "<br>Last Updated on " + result.updatedDateTime.replace('T'," @ ").replace('Z'," ").replace(".",":")+ "<br><br><i> + shows new cases reported yesterday</i>")
+    //.then(result => console.log(result))
+    .then(result => (function(){
+      document.getElementById("country").innerHTML = result.location.countryOrRegion;
+      document.getElementById("totalCases").innerHTML = result.stats.totalConfirmedCases;
+      document.getElementById("totalRecoveredCases").innerHTML = result.stats.totalRecoveredCases;
+      document.getElementById("totalNewCases").innerHTML = result.stats.newlyConfirmedCases;
+    })())
     .catch(error => console.log('error', error));
 
    }
   );
   });
-
-
-
-
-
-  
-}
-
 
