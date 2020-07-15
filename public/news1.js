107146bb6d2704253fd0b7c7377289d567d1e1fb
Barthwal <barthwalumang@gmail.com>
2020-07-15 13:45:32 +0530

    change form card to button in plasma bank page

--- public/news1.js
+++ public/news1.js
@@ -1,4 +1,4 @@
-document.getElementById("news_").classList.add("active")
+//document.getElementById("news_").classList.add("active")
 
 var requestOptions = {
   method: 'GET',
@@ -7,14 +7,15 @@ var requestOptions = {
 var i;
 fetch("https://api.coronatracker.com/news/trending?limit=10&offset&country=Spain&countryCode", requestOptions)
   .then(response => response.json())
-  .then(result => {
-        document.getElementById("news").innerHTML = "";
+  //.then(results => console.log(results))
+  .then(result => (function(){
+        //document.getElementById("news1").innerHTML += '<marquee behavior="scroll" direction="up">';
         for(i = 0; i < 10 ; i++){
-           if(result.items[i].author == ""){
-             result.items[i].author = "Anonymous";
-           }           
-              document.getElementById("news").innerHTML += "<div class=\"item\"><div class=\"content\"><div class=\"ui header center aligned\"><hr>" + result.items[i].title + "<hr></div>" + " <div class=\"meta\"> Author : "+ result.items[i].author + "<br> Published on "+ result.items[i].publishedAt.replace("T", " @ ").replace("Z", " ") +"<br> Published by "+ result.items[i].siteName + "</div><div class=\"description\"><br>" + result.items[i].description + "<br><br>" + result.items[i].content + "<br><br> Read More at : " + "<a href = '" + result.items[i].url + "'>" + result.items[i].url +"</a><br><br></div><div class=\"ui image\"> <img src=\"" + result.items[i].urlToImage+"\"><br><br></div></div></div>";
+          document.getElementById("news1").innerHTML += "<li><span>" + result.items[i].publishedAt + "</span><a href='" + result.items[i].url + "'>" + result.items[i].title + "</a></li>";
         }
 
-  })
+        //document.getElementById("news1").innerHTML += '</marquee>';
+  })())
   .catch(error => console.log('error', error));
+
+  //<marquee behavior="scroll" direction="up">Here is some scrolling text... going up!</marquee>
