af0944d48df5133d3c433a3789b8934df5e62f94
pjflux2001 <pj.flux2001@gmail.com>
2020-07-16 13:43:15 +0530

    flag added, not yet inline

---
+++ public/flag.js
@@ -0,0 +1,25 @@
+var i;
+
+$(function() {
+$.getJSON("https://extreme-ip-lookup.com/json/",
+   function(json){
+    var requestOptions = {
+        method: 'GET',
+        redirect: 'follow'
+    };
+    fetch("https://corona-virus-stats.herokuapp.com/api/v1/cases/countries-search?limit=36&page=1&search="+json.countryCode.toLowerCase()+"&order=total_cases&how=asc", requestOptions)
+  .then(response => response.json())
+  .then(result => 
+    {for(i = 0; i<35 ; i++){
+        if(result.data.rows[i].country_abbreviation==json.countryCode.toUpperCase()){
+            document.getElementById('flag').innerHTML = "<img style = 'max-height: 50px;' src="+result.data.rows[i].flag+">";
+        }
+    }})
+  
+  .catch(error => console.log('error', error));
+  
+                }   
+            );
+});
+
+//"url": "",
--- public/stats.js
+++ public/stats.js
@@ -1,6 +1,5 @@
 var myHeaders = new Headers();
 myHeaders.append("Subscription-Key", "e4db10f1ac324ea7a370a0f2564f800e");
-
 var requestOptions = {
   method: 'GET',
   headers: myHeaders,
@@ -10,7 +9,7 @@ var requestOptions = {
 $(function() {
   $.getJSON("https://extreme-ip-lookup.com/json/",
    function(json) {
-    fetch("https://api.smartable.ai/coronavirus/stats/"+json.countryCode, requestOptions)
+   fetch("https://api.smartable.ai/coronavirus/stats/"+json.countryCode, requestOptions)
     .then(response => response.json())
     //.then(result => x.innerHTML = "Country : "+result.location.countryOrRegion + " [ "+result.location.isoCode +" ]"+"<br>Confirmed : " + result.stats.totalConfirmedCases + " ( +" + result.stats.newlyConfirmedCases +" )"+ '<br>' +"Recovered : " + result.stats.totalRecoveredCases + " ( +" + result.stats.newlyRecoveredCases +" )"+ "<br>Deaths : " + result.stats.totalDeaths + " ( +" + result.stats.newDeaths +" )"+ "<br>Last Updated on " + result.updatedDateTime.replace('T'," @ ").replace('Z'," ").replace(".",":")+ "<br><br><i> + shows new cases reported yesterday</i>")
     //.then(result => console.log(result))
