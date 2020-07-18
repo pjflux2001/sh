96d8ac1c37169a47c689c8372b442a643ac62d04
pjflux2001 <pj.flux2001@gmail.com>
2020-07-18 13:31:55 +0530

    fixed

--- public/news1.js
+++ public/news1.js
@@ -1,16 +1,19 @@
 //document.getElementById("news_").classList.add("active")
+$(function() {
+  $.getJSON("https://extreme-ip-lookup.com/json/",
+     function(json){
 
 var requestOptions = {
   method: 'GET',
   redirect: 'follow'
 };
 var i;
-fetch("https://api.coronatracker.com/news/trending?limit=10&offset&country=Spain&countryCode", requestOptions)
+fetch("https://api.coronatracker.com/news/trending?limit=25&offset&country="+json.country+"&countryCode", requestOptions)
   .then(response => response.json())
   //.then(results => console.log(results))
   .then(result => (function(){
         //document.getElementById("news1").innerHTML += '<marquee behavior="scroll" direction="up">';
-        for(i = 0; i < 10 ; i++){
+        for(i = 0; i < 25 ; i++){
           document.getElementById("news1").innerHTML += "<li class=''><span>" + result.items[i].publishedAt.replace("T"," ").replace("Z"," ") + "</span><br><a href='" + result.items[i].url + "'>" + result.items[i].title + "</a></li>";
         }
 
@@ -19,3 +22,6 @@ fetch("https://api.coronatracker.com/news/trending?limit=10&offset&country=Spain
   .catch(error => console.log('error', error));
 
   //<marquee behavior="scroll" direction="up">Here is some scrolling text... going up!</marquee>
+}   
+            );
+});
