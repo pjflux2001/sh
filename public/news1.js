eb661753acf4a39b43feaf85cb854370d0b5c954
Barthwal <barthwalumang@gmail.com>
2020-07-19 14:54:01 +0530

    add global news without scroller

--- public/news1.js
+++ public/news1.js
@@ -14,7 +14,7 @@
       .then(result => (function(){
             //document.getElementById("news1").innerHTML += '<marquee behavior="scroll" direction="up">';
             for(i = 0; i < 25 ; i++){
-              document.getElementById("news1").innerHTML += "<li class=''><span>" + result.items[i].publishedAt.replace("T"," ").replace("Z"," ") + "</span>| <a href='" + result.items[i].url + "'>" + result.items[i].title + "</a> | "+result.items[i].description+"</li><br><br>";
+              document.getElementById("news1").innerHTML += "<li class=''><span>" + result.items[i].publishedAt.replace("T"," ").replace("Z"," ") + "</span>| <a href='" + result.items[i].url + "'>" + result.items[i].title + "</a> | "+result.items[i].description+"...</li><br><br>";
             }
      
             //document.getElementById("news1").innerHTML += '</marquee>';
