b3d0943f8dedc45d6156345bc49f00bc02ddcd6c
pjflux2001 <pj.flux2001@gmail.com>
2020-07-20 20:53:58 +0530

    email fixed

--- app.js
+++ app.js
@@ -48,14 +48,22 @@ app.get("/contact",function(req,res){
 // https://github.com/sendgrid/sendgrid-nodejs
 app.post("/email",function(req,res){
 	var email =  req.body.email;
+	var name =  req.body.name;
+	var number = req.body.number;
+	var age = req.body.age;
+	var gender = req.body.gender;
+	var country = req.body.country;
+	var state = req.body.state;
+	var city = req.body.city;
+	var bg = req.body.bg;
 	console.log(email);
 	sgMail.setApiKey("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");
 	const msg = {
 	to: email,
 	from: 'pj.flux2001@gmail.com',
 	subject: 'Team BITSians : Success',
-	text: 'Dear User, Your form has been successfully pushed into our database. It will expire in next 72 hours, hence take the required steps to comply with the same. Best Regards, Team BITSians ',
-	html: 'Dear User,<br><br>Your form has been successfully pushed into our database. It will expire in next <strong>72 hours</strong>, hence take the required steps to comply with the same.<br><br> Best Regards,<br> Team BITSians',
+	text: 'Dear '+name+', Your form has been successfully pushed into our database. It will expire in next 72 hours, hence take the required steps to comply with the same. The information that was pushed into this form : Age : <br>Gender : <br>Country : <br>State : <br>City : <br>Blood Group :'+ bg +' We will contact you @'+number+' in case of any emergency. Best Regards, Team BITSians ',
+	html: 'Dear '+name+',<br><br>Your form has been successfully pushed into our database. It will expire in next <strong>72 hours</strong>, hence take the required steps to comply with the same.<br><br>The information that was pushed into this form :<br><p>Age : '+age+'<br>Gender : '+gender+'<br>Country : '+country+'<br>State : '+state+'<br>City : '+city+'<br>Blood Group : '+ bg +'<br></p>We will contact you @'+number+' in case of any emergency.<br><br> Best Regards,<br> Team BITSians',
 	};
 	sgMail.send(msg);
 	res.render("index");
