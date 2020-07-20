e4bdbf9cbf6dceb0522c64f21012ddd68ee6d32f
Barthwal <barthwalumang@gmail.com>
2020-07-21 00:46:00 +0530

    add scroller to global news

--- public/globalnews.js
+++ public/globalnews.js
@@ -11,15 +11,17 @@ var settings = {
     //var $items = $xml.find("channel");
     var $xml = $(response);
     var $items = $xml.find("item");
-
+    
+    $("#globalnews" ).append(`<ul>`);
     $items.each(function(){
 
       var pubDate = $(this).find('pubDate').text();
       var title = $(this).find('title').text();
       var link = $(this).find('link').text();
-      var description = $(this).find('description').text().slice(0,100);
+      var description = $(this).find('description').text().slice(0,88);
 
       $("#globalnews" ).append(`<li>${pubDate} | <a href="${link}">${title}</a> | ${description}...</li><br><br>`);
 
     });
+    $("#globalnews" ).append(`</ul>`);
   });
