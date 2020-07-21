c3048be1cf2b16ecc1314532576b35dfa8a2d19f
Sudhanshu Mohan <sudhanshumohan781@gmail.com>
2020-07-21 23:56:23 +0530

    Search working and removed some extra fields
    Still needs to do more improvements.

--- public/common_pool.js
+++ public/common_pool.js
@@ -58,6 +58,7 @@ adddonorref.on("value", function(snapshot) {
     });
 
 function donorFuzzy(){
+  console.log("HEY DONOR");
   var donorfilter = firebase.database().ref().child("donor");
   donorfilter.on("value", function(snapshot) {
       $("#show-filter").empty();
@@ -65,32 +66,35 @@ function donorFuzzy(){
   
       snapshot.forEach(function(childsnapshot){
         var item = childsnapshot.val();
-        donorHTMLitem += "<div class'patientHTMLitem'><hr><ul></div>";
-        //patientHTMLitem += "<li> Time : <span>"+ item.time + "</span></li>"
-        donorHTMLitem += "<li><b> Date : </b><span>"+ item.date + "</span></li>"
-        //patientHTMLitem += "<li> Contact : <span>"+ item.contact + "</span></li>"
-        donorHTMLitem += "<li><b> Patient Name : </b><span>"+ item.patientName + "</span></li>"
-        donorHTMLitem += "<li><b> Mobile Number : </b><span>"+ item.mobileNumber + "</span></li>"
-        donorHTMLitem += "<li><b> Email : </b><span>"+ item.emailAddress + "</span></li>"
-        donorHTMLitem += "<li><b> Age : </b><span>"+ item.age + "</span></li>"
-        donorHTMLitem += "<li><b> Gender : </b><span>"+ item.gender + "</span></li>"
-        donorHTMLitem += "<li><b> City : </b><span>"+ item.city + "</span></li>"
-        donorHTMLitem += "<li><b> Country : </b><span>"+ item.country + "</span></li>"
-        donorHTMLitem += "<li><b> Blood Group : </b><span>"+ item.bloodGroup + "</span></li>"
-        donorHTMLitem += "<li><b> Diabetes : </b><span>"+ item.diabetes + "</span></li>"
-        donorHTMLitem += "<li><b> Liver Disease : </b><span>"+ item.liver + "</span></li>"
-        donorHTMLitem += "<li><b> Kidney Disease : </b><span>"+ item.kidney + "</span></li>"
-        donorHTMLitem += "<li><b> Blood Pressure Problem : </b><span>"+ item.bloodPressure + "</span></li>"
-        donorHTMLitem += "<li><b> Recovery Date : </b><span>"+ item.recoveryDate + "</span></li>"
-        donorHTMLitem += "</ul></div>";
-  
+        if(filter(item)){
+          donorHTMLitem += "<div class'patientHTMLitem'><hr><ul></div>";
+          //patientHTMLitem += "<li> Time : <span>"+ item.time + "</span></li>"
+          donorHTMLitem += "<li><b> Date : </b><span>"+ item.date + "</span></li>"
+          //patientHTMLitem += "<li> Contact : <span>"+ item.contact + "</span></li>"
+          donorHTMLitem += "<li><b> Patient Name : </b><span>"+ item.patientName + "</span></li>"
+          donorHTMLitem += "<li><b> Mobile Number : </b><span>"+ item.mobileNumber + "</span></li>"
+          donorHTMLitem += "<li><b> Email : </b><span>"+ item.emailAddress + "</span></li>"
+          donorHTMLitem += "<li><b> Age : </b><span>"+ item.age + "</span></li>"
+          donorHTMLitem += "<li><b> Gender : </b><span>"+ item.gender + "</span></li>"
+          donorHTMLitem += "<li><b> City : </b><span>"+ item.city + "</span></li>"
+          donorHTMLitem += "<li><b> Country : </b><span>"+ item.country + "</span></li>"
+          donorHTMLitem += "<li><b> Blood Group : </b><span>"+ item.bloodGroup + "</span></li>"
+          donorHTMLitem += "<li><b> Diabetes : </b><span>"+ item.diabetes + "</span></li>"
+          donorHTMLitem += "<li><b> Liver Disease : </b><span>"+ item.liver + "</span></li>"
+          donorHTMLitem += "<li><b> Kidney Disease : </b><span>"+ item.kidney + "</span></li>"
+          donorHTMLitem += "<li><b> Blood Pressure Problem : </b><span>"+ item.bloodPressure + "</span></li>"
+          donorHTMLitem += "<li><b> Recovery Date : </b><span>"+ item.recoveryDate + "</span></li>"
+          donorHTMLitem += "</ul></div>";
+        }
     });
     $("#show-filter").html(donorHTMLitem);
   });
-      console.log("HEY DONOR");
+      
     }
+
     
 function patientFuzzy(){
+  console.log("HEY Patient");
   var patientfilter = firebase.database().ref().child("patient");
   patientfilter.on("value", function(snapshot) {
       $("#show-filter").empty();
@@ -98,24 +102,38 @@ function patientFuzzy(){
 
       snapshot.forEach(function(childsnapshot){
         var item = childsnapshot.val();
-        patientHTMLitem += "<div class'patientHTMLitem'><hr><ul></div>";
-        //patientHTMLitem += "<li> Time : <span>"+ item.time + "</span></li>"
-        patientHTMLitem += "<li><b> Date : </b><span>"+ item.date + "</span></li>"
-        //patientHTMLitem += "<li> Contact : <span>"+ item.contact + "</span></li>"
-        patientHTMLitem += "<li><b> Patient Name : </b><span>"+ item.patientName + "</span></li>"
-        patientHTMLitem += "<li><b> Mobile Number : </b><span>"+ item.mobileNumber + "</span></li>"
-        patientHTMLitem += "<li><b> Email : </b><span>"+ item.emailAddress + "</span></li>"
-        patientHTMLitem += "<li><b> Age : </b><span>"+ item.age + "</span></li>"
-        patientHTMLitem += "<li><b> Gender : </b><span>"+ item.gender + "</span></li>"
-        patientHTMLitem += "<li><b> Hospital Name : </b><span>"+ item.hospitalName + "</span></li>"
-        patientHTMLitem += "<li><b> City : </b><span>"+ item.city + "</span></li>"
-        patientHTMLitem += "<li><b> Country : </b><span>"+ item.country + "</span></li>"
-        patientHTMLitem += "<li><b> Blood Group : </b><span>"+ item.bloodGroup + "</span></li>"
-        patientHTMLitem += "</ul></div>";
-
-
+        if(filter(item)){
+          patientHTMLitem += "<div class'patientHTMLitem'><hr><ul></div>";
+          //patientHTMLitem += "<li> Time : <span>"+ item.time + "</span></li>"
+          patientHTMLitem += "<li><b> Date : </b><span>"+ item.date + "</span></li>"
+          //patientHTMLitem += "<li> Contact : <span>"+ item.contact + "</span></li>"
+          patientHTMLitem += "<li><b> Patient Name : </b><span>"+ item.patientName + "</span></li>"
+          patientHTMLitem += "<li><b> Mobile Number : </b><span>"+ item.mobileNumber + "</span></li>"
+          patientHTMLitem += "<li><b> Email : </b><span>"+ item.emailAddress + "</span></li>"
+          patientHTMLitem += "<li><b> Age : </b><span>"+ item.age + "</span></li>"
+          patientHTMLitem += "<li><b> Gender : </b><span>"+ item.gender + "</span></li>"
+          patientHTMLitem += "<li><b> Hospital Name : </b><span>"+ item.hospitalName + "</span></li>"
+          patientHTMLitem += "<li><b> City : </b><span>"+ item.city + "</span></li>"
+          patientHTMLitem += "<li><b> Country : </b><span>"+ item.country + "</span></li>"
+          patientHTMLitem += "<li><b> Blood Group : </b><span>"+ item.bloodGroup + "</span></li>"
+          patientHTMLitem += "</ul></div>";
+        }
     });
     $("#show-filter").html(patientHTMLitem);
   });
-  console.log("HEY Patient");
+  
 }
+
+function filter(item){
+    var age = $("#age").val();
+    var bloodGroup = $("#bloodGroup").val();
+    var city = $("#city").val();
+    var state = $("#state").val();
+    var country = $("#country").val();
+    
+    if(item.age ==  age && item.bloodGroup == bloodGroup && item.city == city && item.state == state && item.country == country){
+        return true;
+    } else {
+      return false;
+    }
+  }
