d856a56754fb2ba8bba1056a5a860569ee1c25d3
Barthwal <barthwalumang@gmail.com>
2020-07-22 23:43:28 +0530

    add embeded map to track amenities

--- public/geo.js
+++ public/geo.js
@@ -12,8 +12,8 @@ function getLocation() {
 function showPosition(position) {
 
   browserPos = {lat: position.coords.latitude, lon: position.coords.longitude};
-  x.innerHTML = "&ensp; Your Position -&ensp;Latitude: " + Math.trunc((position.coords.latitude*10000))/10000 + 
-  "&ensp;&&&ensp;Longitude: " + Math.trunc((position.coords.longitude*10000))/10000;
+  x.innerHTML = "&ensp;<strong>Your Position</strong> -&ensp;Latitude: " + Math.trunc((position.coords.latitude*10000))/10000 + 
+  "&ensp;&&ensp;Longitude: " + Math.trunc((position.coords.longitude*10000))/10000;
 }
 
 getLocation();
@@ -25,6 +25,10 @@ var geocoder = platform.getSearchService();
 function geocodeAndSearch(){
   var rad = parseInt(document.getElementById("slider_value").innerHTML);
   var query = document.getElementById("service").value;
+  // embeded map
+  if(query != ""){
+    document.getElementById("embededMap").innerHTML = '<iframe width="100%" height="400px" frameborder="0" style="border:0" src="https://www.google.com/maps/embed/v1/search?q='+ query +'%20nearby&key=AIzaSyACDxf4BL0zC3sYra6Eq72JAjthSwNSoXQ" allowfullscreen></iframe>'
+  }
   let geocodeParam ={
       q: query ,
       in: 'circle:'+ browserPos.lat +',' + browserPos.lon +';r='+rad,
@@ -37,7 +41,7 @@ function geocodeAndSearch(){
       for(let i =0 ; i <result.items.length;i++){
         decky.innerHTML += '<div class="card cardie">'+
          '<div class="card-body text-center">'+
-          '<h3 class="card-title">'+result.items[i].title+'</h3><hr>'+
+          '<h4 class="card-title">'+result.items[i].title+'</h4><hr>'+
            '<p class="card-text"><h5>'+result.items[i].distance/1000+' Kms</h5>'+result.items[i].address.label+'</p>'+
           '</div>' +
           '<div class="card-footer text-center">'+
@@ -46,7 +50,7 @@ function geocodeAndSearch(){
       '  </div>'
         
       }
-      document.getElementById("result_a").innerHTML = "&ensp;"+result.items.length + " " + query +"(s) found in " + rad/1000 + " kilometers around you";        
+      document.getElementById("result_a").innerHTML = "<hr>&ensp;<h3><strong>"+result.items.length + " " + query +"(s) found in " + rad/1000 + " kilometers radius around you.</strong></h3><br><hr>";        
       //info bubble
                
   }
