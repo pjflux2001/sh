313afa5f4b0b796023abf1180739ac840f5b7b11
Sudhanshu Mohan <sudhanshumohan781@gmail.com>
2020-07-24 21:21:01 +0530

    Added Dark Mode
    Added Basic Dark Mode

--- public/design.js
+++ public/design.js
@@ -11,3 +11,26 @@ document.querySelector("body > div.ui.pointing.menu.stackable > div.hamburger").
 		}
 		e.preventDefault();
 	});
+
+	document.addEventListener('DOMContentLoaded', () => {
+
+		const themeStylesheet = document.getElementById('theme');
+		const storedTheme = localStorage.getItem('theme');
+		if(storedTheme){
+			themeStylesheet.href = storedTheme;
+		}
+		const themeToggle = document.getElementById('theme-toggle');
+		themeToggle.addEventListener('click', () => {
+			// if it's light -> go dark
+			if(themeStylesheet.href.includes('light')){
+				themeStylesheet.href = 'dark-theme.css';
+				themeToggle.innerText = 'Light Mode';
+			} else {
+				// if it's dark -> go light
+				themeStylesheet.href = 'light-theme.css';
+				themeToggle.innerText = 'Dark Mode';
+			}
+			// save the preference to localStorage
+			localStorage.setItem('theme',themeStylesheet.href)  
+		})
+	})
